61909152e8c08d50efd959a3e00ee8549685a9df
unknown <salmanabdussalam222001@gmail.com>
2022-06-04 10:11:55 +0700

    anjay

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.get('/', function (req, res) {
   res.json({
     type: 'get',
     number: 1,
-    nama: 'Okyza Maherdy Prabowo',
-    npm: '33220025'
+    nama: 'Salman Abdussalam',
+    npm: '2042031'
   });
 })
 
@@ -14,8 +14,8 @@ app.post('/', function (req, res) {
   res.json({
     type: 'post',
     number: 1,
-    nama: 'Okyza Maherdy Prabowo',
-    npm: '33220025'
+    nama: 'Salman Abdussalam',
+    npm: '2042031'
   });
 })
 
